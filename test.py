9df1343346d5d506f6accca6d24953edda4eae83
Ayan Petlikar <petlikarayan@gmail.com>
2022-06-11 11:54:34 -0400

    better nn

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from os import terminal_size
 import numpy as np
 import pandas as pd
 
@@ -9,17 +8,18 @@ class neuronLayer():
         self.synapticWeights = 2 * np.random.random((inputs, outputs)) - 1
 
 class neuralNet:
-    def __init__(self, layer1, layer2, layer3):
+    def __init__(self, layer1, layer2, layer3, layer4):
         self.layer1 = layer1
         self.layer2 = layer2
         self.layer3 = layer3
+        self.layer4 = layer4
     
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
     def sigmoidDer(self, x):
         return x * (1 - x)
-    
+
     def reLU(self, x):
         return np.maximum(-0.01,x)
 
@@ -28,24 +28,27 @@ class neuralNet:
 
     def train(self, tsi, tso, epochs):
         for i in range(epochs):
-            if((i % 10000) == 0):
+            if((i % 2000) == 0):
                 print(i)
-                seed = np.random.randint(0, 100000)  
+                seed = np.random.randint(10000, 1000000)  
                 np.random.seed(seed)  
                 np.random.shuffle(tsi)
                 np.random.seed(seed)  
                 np.random.shuffle(tso)
 
-            layerOut1, layerOut2, layerOut3 = self.forward(tsi)
+            layerOut1, layerOut2, layerOut3, layerOut4 = self.forward(tsi)
 
-            layerErr3 = tso - layerOut3
+            layerErr4 = tso - layerOut4
+            layerDel4 = layerErr4 * self.sigmoidDer(layerOut4)
+
+            layerErr3 = layerDel4.dot(self.layer4.synapticWeights.T)
             layerDel3 = layerErr3 * self.sigmoidDer(layerOut3)
 
             layerErr2 = layerDel3.dot(self.layer3.synapticWeights.T)
             layerDel2 = layerErr2 * self.sigmoidDer(layerOut2)
 
             layerErr1 = layerDel2.dot(self.layer2.synapticWeights.T)
-            layerDel1 = layerErr1 * self.reLUDer(layerOut1)
+            layerDel1 = layerErr1 * self.sigmoidDer(layerOut1)
 
             layerAdj1 = tsi.T.dot(layerDel1)
             layerAdj2 = layerOut1.T.dot(layerDel2)
@@ -56,32 +59,34 @@ class neuralNet:
             self.layer3.synapticWeights += layerAdj3
 
     def forward(self, inputs):
-        layerOut1 = self.reLUDer(np.dot(inputs, self.layer1.synapticWeights))
+        layerOut1 = self.sigmoid(np.dot(inputs, self.layer1.synapticWeights))
         layerOut2 = self.sigmoid(np.dot(layerOut1, self.layer2.synapticWeights))
         layerOut3 = self.sigmoid(np.dot(layerOut2, self.layer3.synapticWeights))
-        return layerOut1, layerOut2, layerOut3
+        layerOut4 = self.sigmoid(np.dot(layerOut3, self.layer4.synapticWeights))
+        return layerOut1, layerOut2, layerOut3, layerOut4
 
     def test(self, inputs):
-        layerOut1 = self.reLUDer(np.dot(inputs, self.layer1.synapticWeights))
+        layerOut1 = self.sigmoid(np.dot(inputs, self.layer1.synapticWeights))
         layerOut2 = self.sigmoid(np.dot(layerOut1, self.layer2.synapticWeights))
         layerOut3 = self.sigmoid(np.dot(layerOut2, self.layer3.synapticWeights))
+        layerOut4 = self.sigmoid(np.dot(layerOut3, self.layer4.synapticWeights))
 
-        print(f"Output\n {layerOut3}\n")
-        return layerOut3
+        print(f"Output\n {layerOut4}\n")
+        return layerOut4
 
     def weightPri(self):
         print(f"Layer 1 Weights:\n{self.layer1.synapticWeights}\nLayer 2 Weights:\n{self.layer2.synapticWeights}\nLayer 3 Weights:\n{self.layer3.synapticWeights}\n")
 
 if __name__ == '__main__':
-    np.random.seed(1678678)
 
     layer1 = neuronLayer(9, 15)
     layer2 = neuronLayer(15, 10)
-    layer3 = neuronLayer(10, 2)
+    layer3 = neuronLayer(10, 10)
+    layer4 = neuronLayer(10, 2)
 
-    nn = neuralNet(layer1, layer2, layer3)
+    nn = neuralNet(layer1, layer2, layer3, layer4)
 
-    # code: if first or last number is 1 output is 1 else 0
+    # code: if first number is 1 output is 1 else 0
     df = pd.read_csv('hard.csv', delimiter='|', nrows=129, usecols=[0])
     df = df.IN.str.split(',', expand=True)
     trsi = pd.DataFrame(df).to_numpy(dtype=np.intc)
